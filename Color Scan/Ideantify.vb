c308d5fb92be29083d45b66a7874e025403f5a09
Rashan Hasaranga <codexlanka000@gmail.com>
2018-04-15 16:54:54 +0530

    Update Ideantify.vb

--- Color Scan/Ideantify.vb
+++ Color Scan/Ideantify.vb
@@ -57,3 +57,8 @@
 'If CheckColor(Val(ColorRedValue.Value), Val(ColorGreenValue.Value), Val(ColorBlueValue.Value), Val(x1.Text), Val(y1.Text), Val(width1.Text), Val(height1.Text)) Then
            '''
 'End If
+
+
+
+
+
